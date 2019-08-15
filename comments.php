a974c0a01d7b1437f9bb6714154e15bf14512609
Bruno Bernardo <brunob66@gmail.com>
2019-08-15 16:59:42 -0400

    Adding 'Comments'

--- comments.php
+++ comments.php
@@ -1,95 +1,69 @@
-<?php
-/**
- * Template for displaying Comments
- *
- * The area of the page that contains both current comments
- * and the comment form. The actual display of comments is
- * handled by a callback to twentyten_comment which is
- * located in the functions.php file.
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
+<!-- PREVENT ACCESS -->
+
+<?php 
+	if(!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME'])) : 
+		die('You can not access this page directly!'); 
+	endif; 
 ?>
 
-<div id="comments">
-  <?php if ( post_password_required() ) : ?>
-  <p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyten' ); ?></p>
-</div><!-- #comments -->
-<?php
-		/*
-		 * Stop the rest of comments.php from being processed,
-		 * but don't kill the script entirely -- we still have
-		 * to fully load the template.
-		 */
-		return;
-	endif;
+<!-- PASSWORD REQUIRED -->
+
+<?php 
+	if(!empty($post->post_password)) :
+ 		if($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) :
+		endif;
+	endif; 
 ?>
 
-<?php
-	// You can start editing here -- including this comment!
-?>
+<!-- LIST COMMENTS -->
 
-<?php if ( have_comments() ) : ?>
-<h3 id="comments-title">
-  <?php
-			if ( 1 === get_comments_number() ) {
-				printf(
-					/* translators: %s: The post title. */
-					__( 'One Response to %s', 'twentyten' ),
-					'<em>' . get_the_title() . '</em>'
-				);
-			} else {
-				/* translators: %1$s: The number of comments. %2$s: The post title. */
-				printf(
-					_n( '%1$s Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'twentyten' ),
-					number_format_i18n( get_comments_number() ),
-					'<em>' . get_the_title() . '</em>'
-				);
-			}
-			?>
-</h3>
+<?php if($comments) : ?>
 
-<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-<div class="navigation">
-  <div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'twentyten' ) ); ?></div>
-  <div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
-</div> <!-- .navigation -->
-<?php endif; // check for comment navigation ?>
+<h5>Comments: </h5>
 
-<ol class="commentlist">
-  <?php
-					/*
-					 * Loop through and list the comments. Tell wp_list_comments()
-					 * to use twentyten_comment() to format the comments.
-					 * If you want to overload this in a child theme then you can
-					 * define twentyten_comment() and that will be used instead.
-					 * See twentyten_comment() in twentyten/functions.php for more.
-					 */
-					wp_list_comments( array( 'callback' => 'twentyten_comment' ) );
-				?>
-</ol>
 
-<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-<div class="navigation">
-  <div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'twentyten' ) ); ?></div>
-  <div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
-</div><!-- .navigation -->
-<?php endif; // check for comment navigation ?>
+<ul>
+  <?php foreach($comments as $comment) : ?>
 
-<?php
-	/*
-	 * If there are no comments and comments are closed, let's leave a little note, shall we?
-	 * But we only want the note on posts and pages that had comments in the first place.
-	 */
-	if ( ! comments_open() && get_comments_number() ) :
-		?>
-<p class="nocomments"><?php _e( 'Comments are closed.', 'twentyten' ); ?></p>
+  <?php if ($comment->comment_approved == '1') : ?>
+  <p class="mt-3 mb-0 p-0"><?php comment_text(); ?></p>
+  <p class="small mb-3">- <?php comment_type(); ?> by <?php comment_author_link(); ?> on <?php comment_date(); ?> at <?php comment_time(); ?></p>
+  <?php endif; ?>
+
+  <?php endforeach; ?>
+
+</ul>
+
+<?php else : ?>
+
+<p>No comments yet</p>
+
 <?php endif; ?>
 
-<?php endif; // end have_comments() ?>
 
-<?php comment_form(); ?>
+<!-- COMMENT FORM -->
 
-</div><!-- #comments -->
+<?php if(comments_open()) : ?>
+<h5>Leave a comment:</h5>
+<?php if(get_option('comment_registration') && !$user_ID) : ?>
+<p>You must be <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php echo urlencode(get_permalink()); ?>">logged in</a> to post a comment.</p><?php else : ?>
+<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+  <?php if($user_ID) : ?>
+  <p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Log out &raquo;</a></p>
+  <?php else : ?>
+  <p><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" />
+    <label for="author"><small>Name <?php if($req) echo "(required)"; ?></small></label></p>
+  <p><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" />
+    <label for="email"><small>Mail (will not be published) <?php if($req) echo "(required)"; ?></small></label></p>
+  <p><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
+    <label for="url"><small>Website</small></label></p>
+  <?php endif; ?>
+  <p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
+  <p><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
+    <input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" /></p>
+  <?php do_action('comment_form', $post->ID); ?>
+</form>
+<?php endif; ?>
+<?php else : ?>
+<p>The comments are closed.</p>
+<?php endif; ?>
