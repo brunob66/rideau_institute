ad29cfec6655d93b69adfac74328ab228cd003a0
Bruno Bernardo <brunob66@gmail.com>
2019-08-21 15:45:53 -0400

    Added 'Videos' code

--- t-videos.php
+++ t-videos.php
@@ -12,7 +12,8 @@
         <?php
 
 $loop = new WP_Query( array(
-                'post_type' => 'publications',
+                'post_type' => 'videos',
+                'posts_per_page' => 100,
               )
             );
 
@@ -20,12 +21,13 @@ $loop = new WP_Query( array(
             $loop->the_post(); ?>
 
 
-        <div class="col-lg-6">
-          <div class="card mb-4">
+        <div class="col-md-6 mb-4 d-flex align-items-stretch">
+          <div class="card">
             <img src="<?php echo get_the_post_thumbnail_url()?>" alt="" class="card-img-top-publications">
-            <div class="card-body">
+            <div class="card-body d-flex flex-column">
               <h5 class="card-title text-center"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
               <p class="card-text"><?php the_excerpt(); ?></p>
+              <a class="mr-0 ml-auto mt-auto btn btn-primary" href="<?php the_permalink();?>">Watch</a>
             </div>
           </div>
         </div>
